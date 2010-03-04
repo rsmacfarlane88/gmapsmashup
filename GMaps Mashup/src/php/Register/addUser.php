9c145d53c0e3bd42323edebd4c897bdb5dd36357
rsmacfarlane88 <rsmacfarlane88@7430d5e8-232a-11df-8b8d-d39edfa62fdf>
2010-03-04 10:08:44 +0000

    *Fixed the register function so that it warns the user if the  username and email address are already used. Still a bit shaky  but works better than it did before. Could still be better tho

--- GMaps Mashup/src/php/Register/addUser.php
+++ GMaps Mashup/src/php/Register/addUser.php
@@ -8,9 +8,7 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 $email=$_POST['email'];
 
-
-
 $insert = "INSERT INTO users(username,password,email) VALUES ('$username','$password','$email');";
-$resultID = mysql_query($insert, $linkID) or die("Data not found in 'users' table.");
+$resultID = mysql_query($insert) or die("Data not found in 'users' table.");
 
 ?>
