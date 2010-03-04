4028cbc2601b37a10f6804851c49ef158118322a
rsmacfarlane88 <rsmacfarlane88@7430d5e8-232a-11df-8b8d-d39edfa62fdf>
2010-03-04 10:08:44 +0000

    *Fixed the register function so that it warns the user if the  username and email address are already used. Still a bit shaky  but works better than it did before. Could still be better tho

--- trunk/GMaps Mashup/src/php/Register/checkPassword.php
+++ trunk/GMaps Mashup/src/php/Register/checkPassword.php
@@ -4,50 +4,17 @@ include("../functions.php");
 
 $connection = dbConnect();
 
-//asign the data passed from Flex to variables
-$username = $_POST["username"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+$result = mysql_query("select * from users");
 
+echo "<?xml version=\"1.0\" ?><userList>";
 
-//Query the database to see if the given username/password combination is valid.
-$query = mysql_query("SELECT password FROM users WHERE password = '$password'");
-$result = mysql_num_rows($query);
-$query2 = mysql_query("SELECT username FROM users WHERE username = '$username'");
-$result2 = mysql_num_rows($query2);
-$query3 = mysql_query("SELECT email FROM users WHERE email = '$email'");
-$result3 = mysql_num_rows($query3);
-
-
-//start outputting the XML
-$output = "<CheckData><Data>";
-
-
-//if the query returned true, the output <loginsuccess>yes</loginsuccess> else output <loginsuccess>no</loginsuccess>
-if($result > 0)
+while($row = mysql_fetch_assoc($result))
 {
-	$output .= "<password>yes</password>";		
-}else{
-	$output .= "<password>no</password>";	
+	echo "<user><username>" . $row["username"] . "</username>";
+	echo "<password>" . $row["password"] . "</password>";
+	echo "<email>" . $row["email"] . "</email></user>";
 }
 
-if($result2 > 0)
-{
-	$output .= "<username>yes</username>";
-}else{
-	$output .= "<username>no</username>";
-}
-
-if($result3 > 0)
-{
-	$output .= "<email>yes</email>";
-}else{
-	$output .= "<email>no</email>";
-}
-
-$output .= "</Data></CheckData>";
-
-//output all the XML
-print ($output);
+echo "</userList>";
 
 ?>
